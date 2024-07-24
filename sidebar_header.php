016d99622c734d7332932d0369674b25e8b51324
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-07-24 19:14:59 +0530

    UPDATE V0.3

--- sidebar_header.php
+++ sidebar_header.php
@@ -2,10 +2,10 @@
     <!-- Logo -->
     <a href="index.php" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><img src="icon/favicon.png" alt=""></span>
+        <span class="logo-mini"><img src="icon/siyarata2.png" alt=""></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg dark"><img src="icon/logo dark.png" alt=""></span>
-        <span class="logo-lg light"><img src="icon/logo dark.png" alt=""></span>
+        <span class="logo-lg dark"><img src="icon/siyarata.png" alt=""></span>
+        <span class="logo-lg light"><img src="icon/siyarata.png" alt=""></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
